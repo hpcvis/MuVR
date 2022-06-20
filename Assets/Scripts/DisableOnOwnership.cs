fc4e2f2e7b5b3040dd736c12c3aa5ba1198ad347
Joshua Dahl <joshuadahl@nevada.unr.edu>
2022-06-20 11:52:28 -0700

    Disable On Ownership
    Added a component to disable gameObjects/components based on the current ownership status.

---
+++ Assets/Scripts/DisableOnOwnership.cs
@@ -0,0 +1,39 @@
+using FishNet.Connection;
+using UnityEngine;
+
+// Component that enables or disables a GameObject or list of Components based on the current ownership status of this object
+public class DisableOnOwnership : EnchancedNetworkBehaviour {
+	[Tooltip("Array of components to disable when ownership changes")]
+	public Component[] toDisable;
+	[Tooltip("When true components will be disabled while the owner, when false the components will be disabled while not the owner")]
+	public bool disableWhenOwner = true;
+	[Tooltip("When true the attached game object will be disabled instead of individual components")]
+	public bool disableGameObject = false;
+
+	// When the ownership of the object changes, figure out which elements should be enabled
+	public override void OnOwnershipBoth(NetworkConnection prev) {
+		base.OnOwnershipBoth(prev);
+
+		// Determine if we should enable or disabled based on the user's configuration
+		bool shouldEnable = !(IsOwner ? disableWhenOwner : !disableWhenOwner);
+
+		// If we are disabling the entire game object, enable or disable the entire game object
+		if (disableGameObject) {
+			gameObject.SetActive(shouldEnable);
+			return;
+		}
+		
+		// Otherwise... for each of the components we wish to disable, find its enabled property...
+		foreach (var component in toDisable) {
+			var properties = component.GetType().GetProperties();
+			foreach(var property in properties)
+				// If the enabled property exists on the component, enable or disable the component
+				if (property.Name == "enabled") {
+					property.SetValue(component, shouldEnable);
+					break;
+				}
+		}
+				
+	}
+
+}
