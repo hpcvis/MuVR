03d36bfa460459737d0f382d6a0ad3c99f20543d
Joshua Dahl <joshuadahl@nevada.unr.edu>
2022-06-29 10:57:34 -0700

    Ping Pong Scoring
    Updated the Ping-Pong demo to support scoring and respawning the ball when it goes out of bounds.

---
+++ Assets/Samples/PingPong/Scripts/LookAtCamera.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+// Component that causes this object to rotate so it is always facing the camera
+public class LookAtCamera : MonoBehaviour {
+	[Tooltip("Camera to look at (will default to Camera.current if not set)")]
+	public Camera target = null;
+	[Tooltip("Prevent rotation along one of the Euler axis")]
+	public bool lockX = false, lockY = false, lockZ = false;
+
+	// Update is called once per frame
+	private void Update() {
+		var position = Camera.current?.transform.position ?? transform.position;
+		try {
+			position = target.transform.position;
+		} catch (UnassignedReferenceException) {}
+
+		// Don't update rotation if it would be null
+		if (position == transform.position) return;
+		
+		// Invert the position so that text remains upright
+		var rot = Quaternion.LookRotation(transform.position - position, Vector3.up);
+		
+		// Make sure that any locked axis maintain their old values
+		var oldRot = transform.rotation;
+		if (lockX) rot.eulerAngles = new Vector3(oldRot.eulerAngles.x, rot.eulerAngles.y, rot.eulerAngles.z);
+		if (lockY) rot.eulerAngles = new Vector3(rot.eulerAngles.x, oldRot.eulerAngles.y, rot.eulerAngles.z);
+		if (lockZ) rot.eulerAngles = new Vector3(rot.eulerAngles.x, rot.eulerAngles.y, oldRot.eulerAngles.z);
+
+		// Update the rotation
+		transform.rotation = rot;
+	}
+}
---
+++ Assets/Samples/PingPong/Scripts/PingPongGameManager.cs
@@ -0,0 +1,74 @@
+using FishNet.Object;
+using FishNet.Object.Synchronizing;
+using TMPro;
+using UnityEngine;
+
+public class PingPongGameManager : EnchancedNetworkBehaviour {
+	[SyncVar(OnChange = nameof(UpdateScores))]
+	public int plusScore = 0;
+	[SyncVar(OnChange = nameof(UpdateScores))]
+	public int minusScore = 0;
+	
+	[SerializeField] private NetworkObject ballPrefab;
+	[SerializeField] private Transform plusSpawn, minusSpawn;
+	[SerializeField] private TextMeshPro text;
+
+	private bool spawnPlus = false;
+
+	// Randomly chose a spawn location for the ball on the server
+	public override void OnStartServer() {
+		base.OnStartServer();
+
+		spawnPlus = (Random.Range(0f, 1f) > .5f);
+		RespawnBall();
+	}
+
+	// Update the scores on the client when they join
+	public override void OnStartClient() {
+		base.OnStartClient();
+		UpdateScores(0, 0, false);
+	}
+
+	// Function that respawns the ball at the position specified by <spawnPlus>
+	[Server]
+	public void RespawnBall() {
+		var ball = Instantiate(ballPrefab, spawnPlus ? plusSpawn.position : minusSpawn.position, Quaternion.identity);
+		Spawn(ball.gameObject);
+	}
+
+	// Function called when an object goes out of bounds on the +x side
+	public void OnObjectOutOfBoundsPlus(Collider ball) {
+		if (ball.CompareTag("PingPongBall")) {
+			if(IsServer) OnBallOutOfBounds(ball.GetComponent<NetworkObject>(), true);
+		}
+	}
+	
+	// Function called when an object goes out of bounds on the -x side
+	public void OnObjectOutOfBoundsMinus(Collider ball) {
+		if (ball.CompareTag("PingPongBall")) {
+			if(IsServer) OnBallOutOfBounds(ball.GetComponent<NetworkObject>(), false);
+		}
+	}
+
+	// Function called on the server when the ball goes out of bounds
+	[Server]
+	private void OnBallOutOfBounds(NetworkObject ball, bool plusOut) {
+		// Increment score 
+		if (plusOut) minusScore++;
+		else plusScore++;
+
+		// Respawn ball on the losing side
+		spawnPlus = plusOut;
+		
+		// Destroy then respawn the ball
+		Destroy(ball.gameObject);
+		RespawnBall();
+	}
+	
+	// Function called when one of the score variables is changed, updates the score text
+	private void UpdateScores(int old, int @new, bool asServer) {
+		// Bold the local player's score (the position of the camera will either be positive or negative)
+		text.text = (Camera.current?.transform.position.x ?? 0) > 0 ?
+			$"<b>Plus's Score: {plusScore}</b>\nMinus's Score: {minusScore}" : $"Plus's Score: {plusScore}\n<b>Minus's Score: {minusScore}</b>";
+	}
+}
