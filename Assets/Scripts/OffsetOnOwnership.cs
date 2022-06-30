2988f6cc74bda032b932a71824aa0fe254686dd3
Joshua Dahl <joshuadahl@nevada.unr.edu>
2022-06-30 13:58:15 -0700

    Offset on Ownership Tweaks
    Offset on Ownership now moves transforms instead of SyncPoses. It is now intended to offset child objects.

--- Assets/Scripts/OffsetOnOwnership.cs
+++ Assets/Scripts/OffsetOnOwnership.cs
@@ -1,22 +1,25 @@
 using FishNet.Connection;
 using UnityEngine;
 
-// Component that changes a sync pose's offset based on its ownership status
-public class OffsetSyncPoseOnOwnership : EnchancedNetworkBehaviour {
+// Component that changes a child transform's offset based on its ownership status
+public class OffsetOnOwnership : EnchancedNetworkBehaviour {
 	// The SyncPose to be updated
-	public SyncPose target;
+	public Transform target;
 	// The offsets when the object is owned or not
 	public Pose ownedOffset = Pose.identity, unownedOffset = Pose.identity;
 
 	// Automatically set target equal to a SyncPose on the same object
-	new void OnValidate() {
+	private new void OnValidate() {
 		base.OnValidate();
-		target ??= GetComponent<SyncPose>();
+		target ??= GetComponent<Transform>();
 	}
 
 	// When ownership changes set the pose appropriately
 	public override void OnOwnershipBoth(NetworkConnection prev) {
 		base.OnOwnershipBoth(prev);
-		target.offset = IsOwner ? ownedOffset : unownedOffset;
+		var offset = IsOwner ? ownedOffset : unownedOffset;
+		
+		target.position = offset.position;
+		target.rotation = offset.rotation;
 	}
 }
