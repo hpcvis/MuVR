768ef5a47b4e07e2c98e7f659cdc32622d148492
Joshua Dahl <joshuadahl@nevada.unr.edu>
2022-07-12 12:38:14 -0700

    Ownership Fixes
    Fixed a bug where the ownership manager would sometimes not update the objects owner properly, and it would sit in an ownership volume with the wrong owner.

--- Assets/Scripts/OwnershipManager.cs
+++ Assets/Scripts/OwnershipManager.cs
@@ -1,5 +1,6 @@
 using FishNet.Connection;
 using FishNet.Object;
+using TriInspector;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
@@ -11,14 +12,18 @@ public class OwnershipManager : EnchancedNetworkBehaviour {
 	public bool enableVolumeTransfer = true;
 	[PropertyTooltip("Should the owner of this object return it to the scene before leaving the game?")]
 	public bool releaseOwnershipOnLeave = true;
-
+	
+	
+	[PropertyTooltip("XR Interactable that is interacted with to trigger interactions")]
+	[ShowIf(nameof(enableInteractionTransfer)), PropertyOrder(1)]
+	public XRBaseInteractable interactable = null;
+	[PropertyTooltip("Amount of time to wait before an ownership transfer can occur again")]
+	public float ownershipTransferCooldown = .1f; 
+	
 	// Counter tracking how many controllers are actively selecting us
 	private uint selectionCount = 0;
 	// Property indicating if we are actively selected
 	private bool isSelected => selectionCount > 0;
-
-	// XR Interactable that is interacted with to trigger interactions
-	public XRBaseInteractable interactable = null;
 
 	// When this object is spawned on the client, add it as a listener to the interaction's interactions
 	public override void OnStartClient() {
@@ -71,55 +76,38 @@ public class OwnershipManager : EnchancedNetworkBehaviour {
 	}
 
 	// When this object is interacted with (only called if interaction transfers are enabled), give it to the interaction's owner
-	void OnInteractableSelected(SelectEnterEventArgs e) {
+	protected void OnInteractableSelected(SelectEnterEventArgs e) {
 		// note: beware of NetworkObjects that may be in the way of the user representation that we are looking for
 		// there was a NetworkObject on the XRRig at some point which broke this whole function
 		var no = e.interactorObject.transform.GetComponentInParent<NetworkObject>();
 		if (no is null) return;
 
-		GiveOwnershipWithCooldown(no.Owner);
+		GiveOwnershipWithCooldown(no.Owner, ownershipTransferCooldown);
 		selectionCount++; // Since we are now selected, volume transfers are temporarily disabled
 	}
 	
 	// When interaction with this object ceases, decrement the number of selections
-	void OnInteractableUnselected(SelectExitEventArgs e) {
+	protected void OnInteractableUnselected(SelectExitEventArgs e) {
 		selectionCount--; // If this was the last interaction, volume transfers are now enabled again!
-		
-		// If we are no longer selected by anyone, transfer our ownership to whoever controls the OwnershipVolume we are within
-		if(!isSelected)
-			if(enableVolumeTransfer && lastVolume is not null) OnTriggerEnter(lastVolume.GetComponent<Collider>());
 	}
 
 	// When this object enters an Ownership Volume (only called if volume transfers are enabled), give it to the volume's owner
-	private OwnershipVolume lastVolume; // Variable tracking the last OwnershipVolume we were within (used to set owner when interaction stops) NOTE: Overlapping ownership volumes will act strangely
-	void OnTriggerEnter(Collider other) {
+	protected void OnTriggerStay(Collider other) {
 		if (!enableVolumeTransfer) return;
-
+		
 		var ov = other.GetComponent<OwnershipVolume>();
 		if (ov is null) return;
-		lastVolume = ov; // Save a reference to the volume for latter
-		
+		if (ov.volumeOwner == Owner) return; // No need to transfer if the volume has the same owner
+
 		// If we are currently selected don't transfer ownership
 		if (isSelected) return;
 		
-		if (ov.volumeOwner is not null)
-			GiveOwnershipWithCooldown(ov.volumeOwner);
+		// Debug.Log($"{this} - {ov}");
+
+		if (ov.volumeOwner is not null) 
+			GiveOwnershipWithCooldown(ov.volumeOwner, ownershipTransferCooldown);
+
 		// Be sure to listen for changes in ownership
 		ov.RegisterAsListener(this);
-	}
-
-	// When this object leaves its Ownership Volume it stops listening to changes in ownership
-	void OnTriggerExit(Collider other) {
-		if (!enableVolumeTransfer) return;
-
-		var ov = other.GetComponent<OwnershipVolume>();
-		if (ov is null) return;
-		if (ov == lastVolume) lastVolume = null; // We are no longer colliding with the previous volume
-		
-		// If we are currently selected don't transfer ownership
-		if (isSelected) return;
-
-		// Stop listening to changes in ownership
-		ov.UnregisterAsListener(this);
 	}
 }
