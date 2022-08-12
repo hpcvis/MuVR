b3056260d62c77a618613fe73b1c6cde71ff5188
Joshua Dahl <joshuadahl@nevada.unr.edu>
2022-08-12 10:53:09 -0700

    FishNet Submodule
    Fish-Networking is now included as a submodule!

--- Assets/Thirdparty/FishNet/Runtime/Broadcast/IBroadcast.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace FishNet.Broadcast
-{
-    /// <summary>
-    /// Include this interface on types intended to be used with Broadcast.
-    /// </summary>
-    public interface IBroadcast { }
-}
--- Assets/Thirdparty/FishNet/Runtime/Documenting/Attributes.cs
+++
@@ -1,8 +0,0 @@
-﻿
-using System;
-
-namespace FishNet.Documenting
-{
-    public class APIExcludeAttribute : Attribute { }
-
-}
