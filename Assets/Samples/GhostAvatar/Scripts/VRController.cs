effe142dd14648a62922c16ceb9d7bdc60239b96
Joshua Dahl <joshuadahl@nevada.unr.edu>
2022-11-02 14:27:50 -0700

    Speed Based VR Controller
    Added some code to dynamically adjust how closely the legs should follow the body based on the speed of the hips (slower speeds result in less accurate tracking.)

--- Assets/Samples/GhostAvatar/Scripts/VRController.cs
+++ Assets/Samples/GhostAvatar/Scripts/VRController.cs
@@ -1,13 +1,15 @@
-﻿using MuVR.Enhanced;
+﻿using CircularBuffer;
+using MuVR.Enhanced;
 using UltimateXR.Avatar;
 using UltimateXR.Core;
 using UnityEngine;
 
 public class VRController : PFNN.Controller {
-
 	public Transform Hips, HMD, Foot;
-	public float targetDistance = .13f;
-	private float initialHMDHeight = 1.5f;
+	private readonly float initialHMDHeight = 1.5f;
+
+	// Buffer tracking positions of the headset over time
+	private readonly CircularBuffer<Vector3> hipPositionBuffer = new(10);
 
 	private void OnEnable() {
 		UxrManager.AvatarMoved += OnAvatarMoved;
@@ -15,18 +17,28 @@ public class VRController : PFNN.Controller {
 		// Teleport the legs under the avatar
 		TeleportLegs();
 	}
-	
 	private void OnDisable() {
 		UxrManager.AvatarMoved -= OnAvatarMoved;
 	}
 
 	protected override void Update() {
+		// Add the new hip position to the buffer
+		hipPositionBuffer.PushBack(Hips.position);
+		// Calculate the average speed of the headset
+		var speed = 0f;
+		for (var i = 1; i < hipPositionBuffer.Size; i++)
+			speed += (hipPositionBuffer[i] - hipPositionBuffer[i - 1]).magnitude;
+		speed /= hipPositionBuffer.Size;
+
+		// Adjust the target distance based on the speed of the hips 
+		var targetDistance = Mathf.Max(-150 * speed + .3f, 0);
+
 		// Make sure the body is always under the HMD (using strafing)
 		MoveCharacterTo(Hips.transform.forward, Hips.transform.position, 0, 1, targetDistance);
 
 		const float Cmax = .95f;
 		const float Cmid = .85f;
-		crouchedTarget = Mathf.Clamp01( 1 - ((HMD.transform.position.y - Foot.transform.position.y) / (Cmax * initialHMDHeight - Cmid * initialHMDHeight) - Cmid / (Cmax - Cmid)) );
+		crouchedTarget = Mathf.Clamp01(1 - ((HMD.transform.position.y - Foot.transform.position.y) / (Cmax * initialHMDHeight - Cmid * initialHMDHeight) - Cmid / (Cmax - Cmid)));
 
 		base.Update();
 	}
@@ -34,19 +46,18 @@ public class VRController : PFNN.Controller {
 	protected void TeleportLegs() {
 		initialWorldPosition = Hips.transform.position.FixedHeight(0);
 		initialWorldPosition.x += 3f; // Not entirely sure why this is necessary...
+		// Add a bunch of large data to the hip position buffer so that we ensure the legs get close to the user
+		for(var i = 0; i < 10; i++)
+			hipPositionBuffer.PushBack(Vector3.one * i);
 		ResetCharacter();
 	}
-	
+
 	protected void OnAvatarMoved(object sender, UxrAvatarMoveEventArgs e) {
 		// If the magnitude is large, that means we teleported and thus the legs should teleport as well
-		if ((e.OldPosition - e.NewPosition).sqrMagnitude > 2 * 2) {
-			TeleportLegs();
-			// Debug.Log("Teleported");
-			//
-			// Debug.Log(e.NewPosition);
-			// Debug.Log(GetJoint(JointType.Hips).jointPoint.transform.position);
-		}
+		if ((e.OldPosition - e.NewPosition).sqrMagnitude > 2 * 2) TeleportLegs();
+		// Debug.Log("Teleported");
+		//
+		// Debug.Log(e.NewPosition);
+		// Debug.Log(GetJoint(JointType.Hips).jointPoint.transform.position);
 	}
-	
-	
 }
