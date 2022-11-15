1dd1a44ac287cb0e3d15c12f9361093771e48cd1
Joshua Dahl <joshuadahl@nevada.unr.edu>
2022-11-14 16:00:29 -0800

    Leg Direction Fix
    The Legs are now allowed to rotate if they are facing in a substantially different direction from the hips.

--- Assets/Scripts/MuVR/Enhanced/MathExtensions.cs
+++ Assets/Scripts/MuVR/Enhanced/MathExtensions.cs
@@ -18,6 +18,11 @@ namespace MuVR.Enhanced {
 
 		public static Vector2 ToVec2(this Vector3 vec) => new Vector2(vec.x, vec.y);
 		public static Vector2 ToVec2(this Vector4 vec) => new Vector2(vec.x, vec.y);
+
+		public static bool Approximately(this Vector2 a, Vector2 b, float _epsilon) {
+			return (a - b).magnitude < _epsilon;
+		}
+		public static bool Approximately(this Vector2 a, Vector2 b) => Approximately(a, b, Mathf.Epsilon);
 	}
 
 	public static class Vector3Extensions {
@@ -25,6 +30,11 @@ namespace MuVR.Enhanced {
 		
 		public static Vector3 ToVec3(this Vector2 vec, float z = 0) => new Vector3(vec.x, vec.y, z);
 		public static Vector3 ToVec3(this Vector4 vec) => new Vector3(vec.x, vec.y, vec.z);
+		
+		public static bool Approximately(this Vector3 a, Vector3 b, float _epsilon) {
+			return (a - b).magnitude < _epsilon;
+		}
+		public static bool Approximately(this Vector3 a, Vector3 b) => Approximately(a, b, Mathf.Epsilon);
 
 		public static Quaternion AngularVelocityToQuaternion(this Vector3 v) => Quaternion.AngleAxis(v.magnitude, v.normalized);
 	}
@@ -32,6 +42,11 @@ namespace MuVR.Enhanced {
 	public static class Vector4Extensions {
 		public static Vector4 ToVec4(this Vector2 vec, float z = 0, float w = 0) => new Vector4(vec.x, vec.y, z, w);
 		public static Vector4 ToVec4(this Vector3 vec, float w = 0) => new Vector4(vec.x, vec.y, vec.z, w);
+		
+		public static bool Approximately(this Vector4 a, Vector4 b, float _epsilon) {
+			return (a - b).magnitude < _epsilon;
+		}
+		public static bool Approximately(this Vector4 a, Vector4 b) => Approximately(a, b, Mathf.Epsilon);
 	}
 
 	public static class PoseExtensions {
--- Assets/Scripts/MuVR/VRController.cs
+++ Assets/Scripts/MuVR/VRController.cs
@@ -21,6 +21,9 @@ public class VRController : PFNN.Controller {
 		UxrManager.AvatarMoved -= OnAvatarMoved;
 	}
 
+	[SerializeField] private float targetDistance = 0;
+	private float targetDistanceTarget;
+
 	protected override void Update() {
 		// Add the new hip position to the buffer
 		hipPositionBuffer.PushBack(Hips.position);
@@ -30,10 +33,19 @@ public class VRController : PFNN.Controller {
 			speed += (hipPositionBuffer[i] - hipPositionBuffer[i - 1]).magnitude;
 		speed /= hipPositionBuffer.Size;
 
-		// Adjust the target distance based on the speed of the hips 
-		var targetDistance = Mathf.Max(-150 * speed + .3f, 0);
+		// While the feet and HMD are pointing in substantially different directions force the target distance to 0
+		var footForward = ((GetJoint(JointType.LeftToeBase).jointPoint.transform.forward + GetJoint(JointType.RightToeBase).jointPoint.transform.forward) / 2).normalized.FixedHeight(0);
+		if (Mathf.Abs(Vector3.Angle(footForward, HMD.forward.FixedHeight(0))) > 10) speed = 1;
 
-		// Make sure the body is always under the HMD (using strafing)
+		// Adjust the target distance based on the speed of the hips 
+		targetDistanceTarget = Mathf.Max(-150 * speed + .3f, 0);
+		// Debug.Log($"{footForward} - {Hips.forward.FixedHeight(0)} - {Mathf.Abs(Vector3.Angle(footForward, HMD.forward.FixedHeight(0)))} - {targetDistance}");
+
+		// Allow the target distance to immediately drop to match its target, but force it to take 3 seconds to reach its maximum
+		if (targetDistance < targetDistanceTarget) targetDistance += .1f * Time.deltaTime;
+		if (targetDistance > targetDistanceTarget) targetDistance = targetDistanceTarget;
+
+		// Make sure the body is always under the hips (using strafing)
 		MoveCharacterTo(Hips.transform.forward, Hips.transform.position, 0, 1, targetDistance);
 
 		const float Cmax = .95f;
