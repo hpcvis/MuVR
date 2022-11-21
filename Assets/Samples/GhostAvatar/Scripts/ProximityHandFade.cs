7652a537a942dfe9020b5953bf139c120ee7e71d
Joshua Dahl <joshuadahl@nevada.unr.edu>
2022-11-21 13:21:56 -0800

    Hand Fading
    Split the avatar's hands into a left and right half. Added some code to fade away the ghost hands when they are close to the physical hands.

---
+++ Assets/Samples/GhostAvatar/Scripts/ProximityHandFade.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+// Class which fades away the ghost hand when it is close to the physical hand
+[RequireComponent(typeof(SkinnedMeshRenderer))]
+public class ProximityHandFade : MonoBehaviour {
+	private SkinnedMeshRenderer renderer;
+	private Material mat;
+	private float originalAlpha;
+	private void Awake() {
+		renderer = GetComponent<SkinnedMeshRenderer>();
+		mat = new Material(renderer.material);
+		renderer.material = mat;
+		originalAlpha = mat.color.a;
+	}
+
+	// The bones to check the distance of
+	public Transform physicsBone, ghostBone;
+	// When the bones are further than this distance away, make the hand "completely" opaque
+	public float distanceThreshold = .1f;
+
+	public void Update() {
+		var color = mat.color;
+		color.a = Mathf.Min((physicsBone.position - ghostBone.position).magnitude / distanceThreshold, 1) * originalAlpha;
+		mat.color = color;
+	}
+}
