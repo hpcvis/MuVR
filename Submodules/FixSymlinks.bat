c478154fc5030ebc0e0566da248c527950bcadf7
hpcvis <77296871+hpcvis@users.noreply.github.com>
2022-11-30 13:46:10 -0800

    Update FixSymlinks.bat

--- Submodules/FixSymlinks.bat
+++ Submodules/FixSymlinks.bat
@@ -1,9 +1,9 @@
 REM delete the old links/files if links haven't been setup yet
-rmdir "%~dp0..\Assets\Thirdparty\FishNet"
-IF exist "%~dp0..\Assets\Thirdparty\FishNet" del "%~dp0..\Assets\Thirdparty\FishNet"			REM delete it as a file if a folder doesn't yet exist
-rmdir "%~dp0..\Assets\Thirdparty\UltimateXR"
-IF exist "%~dp0..\Assets\Thirdparty\UltimateXR" del "%~dp0..\Assets\Thirdparty\UltimateXR"		REM delete it as a file if a folder doesn't yet exist
+rmdir "..\Assets\Thirdparty\FishNet"
+IF exist "..\Assets\Thirdparty\FishNet" del "..\Assets\Thirdparty\FishNet"			REM delete it as a file if a folder doesn't yet exist
+rmdir "..\Assets\Thirdparty\UltimateXR"
+IF exist "..\Assets\Thirdparty\UltimateXR" del "..\Assets\Thirdparty\UltimateXR"		REM delete it as a file if a folder doesn't yet exist
 
 REM create new links
-mklink /D "%~dp0..\Assets\Thirdparty\FishNet" "%~dp0\FishNet\Assets\FishNet" 
-mklink /D "%~dp0..\Assets\Thirdparty\UltimateXR" "%~dp0\UltimateXR" 
+mklink /d "..\Assets\Thirdparty\FishNet" "..\..\Submodules\FishNet\Assets\FishNet" 
+mklink /d "..\Assets\Thirdparty\UltimateXR" "..\..\Submodules\UltimateXR" 
